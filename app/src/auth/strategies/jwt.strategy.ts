70a0ca50033e72edf29548b8e3ccbba01b0c2442
Alex Tuirán <alextuiran8@gmail.com>
2025-12-12 22:19:12 -0500

    feat(auth): Implemented jwt strategies for get token and build request

---
+++ app/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { AuthUser } from '../interfaces/auth-user.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly config: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: config.getOrThrow<string>('JWT_SECRET'),
+        });
+    }
+    async validate(payload: JwtPayload): Promise<AuthUser> {
+        return payload;
+    }
+}
