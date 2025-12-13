d7dbf495f5e6a701896f32af3bef57c17908e674
Alex Tuirán <alextuiran8@gmail.com>
2025-12-13 14:00:36 -0500

    feat(swagger): Corrected Meta Body for include endpoint path

--- app/src/swagger/dto/api-success-res.dto.ts
+++ app/src/swagger/dto/api-success-res.dto.ts
@@ -3,6 +3,9 @@ import { ApiProperty } from '@nestjs/swagger';
 export class ApiMetaDto {
     @ApiProperty({ example: '2025-12-12T12:00:00.000Z' })
     timestamp!: string;
+
+    @ApiProperty({ example: 'auth/register'})
+    path!: string;
 }
 
 export class SuccessResponseDto {
