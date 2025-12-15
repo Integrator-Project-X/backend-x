0a97041d9eaeb3edf9ebc63b79be2ccb994146dc
Alex Tuirán <alextuiran8@gmail.com>
2025-12-15 10:37:10 -0500

    ref(diagnosis): corrected incorrect decorator in Description field in diagnosis.dto.ts

--- app/src/diagnosis/dto/diagnosis.dto.ts
+++ app/src/diagnosis/dto/diagnosis.dto.ts
@@ -1,15 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, Min, IsBoolean, IsOptional } from 'class-validator';
+import { IsInt, Min, IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class DiagnosisDto {
-    @ApiProperty({
-        example: 1,
-        description: 'Diagnosis ID associated with the diagnosis',
-    })
-    @IsInt()
-    @Min(1)
-    id_diagnosis: number;
-
     @ApiProperty({
         example: 2,
         description: 'Personal ID associated with the diagnosis',
@@ -22,15 +14,10 @@ export class DiagnosisDto {
         example: 'Example diagnosis description',
         description: 'Description of the diagnosis',
     })
-    @IsBoolean()
+    @IsString()
     description: string;
 
-    @ApiProperty({
-        example: true,
-        description: 'Indicates if the diagnosis is active',
-        default: true,
-        required: false,
-    })
+    @ApiProperty({ example: true, required: false, default: true })
     @IsBoolean()
     @IsOptional()
     isActive?: boolean = true;
